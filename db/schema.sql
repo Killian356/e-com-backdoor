52cbc2e04c2286945aa19eb6bb0713519907a068
Killian Chamberlain <killian7195@gmail.com>
2022-01-30 22:12:03 -0600

    adjusted database, fixed .env file

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,3 @@
-DROP DATABASE IF EXISTS ecommerce_db;
-CREATE DATABASE ecommerce_db;
-USE ecommerce_db;
+DROP DATABASE IF EXISTS e_com_backdoor_db;
+CREATE DATABASE e_com_backdoor_db;
+USE e_com_backdoor_db;
